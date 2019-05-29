5b171c595276f05e49f0e416aa7e327cb948f340
Matthew McRaven <35609985+Matthew-McRaven@users.noreply.github.com>
2019-05-28 21:15:10 -0700

    Fix typo in appimagetool url.
    Fix mistake where appimagetool url is incorrect, which caused config.sh to fail.

--- config.sh
+++ config.sh
@@ -6,12 +6,12 @@ wget https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2
 tar xf patchelf-0.9.tar.bz2
 ( cd patchelf-0.9/ && ./configure  && make && sudo make install )
 rm patchelf-0.9.tar.bz2
-sudo wget -c "https://github.com/AppImage/AppImageKit/releases/download/continu$
+sudo wget -c "https://github.com/AppImage/AppImageKit/releases/download/continunuous/appimagetool-x86_64.AppImage" -O /usr/local/bin/appimagetool
 sudo chmod a+x /usr/local/bin/appimagetool
 cd linuxdeployqt
 red=`tput setaf 1`
-echo -e "${red}All linuxdeployqt dependencies have been collected.\n"
-echo -e "${red}To build linuxdeployqt, open the directory's .pro file in Qt Creator.\n"
-echo -e "${red}For each Qt build kit that you would like to enable linuxdeployqt on,\n"
-echo -e "${red}build the project in Qt creator, and then open the build directory in the terminal.\n"
+echo -e "${red}All linuxdeployqt dependencies have been collected."
+echo -e "${red}To build linuxdeployqt, open the directory's .pro file in Qt Creator."
+echo -e "${red}For each Qt build kit that you would like to enable linuxdeployqt on,"
+echo -e "${red}build the project in Qt creator, and then open the build directory in the terminal."
 echo -e "${red}In the terminal, execute \"sudo make install\"."
