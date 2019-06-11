13d924dd97c16dec50d3cffac75573998bbbaabe
Matthew McRaven <matthew.mcraven@pepperdine.edu>
2019-06-11 15:42:13 -0700

    Refactor execution statistic calculation.
    Refactor execution statistic calculation because it was off-by-one.
    No longer display unused opcodes or addressing modes.

--- pep9asm/executionstatisticswidget.cpp
+++ pep9asm/executionstatisticswidget.cpp
@@ -36,47 +36,98 @@ void ExecutionStatisticsWidget::onClear()
 
 void ExecutionStatisticsWidget::onSimulationStarted()
 {
+    // Make sure no statistics are displayed at the start of a run.
     onClear();
 }
 
 void ExecutionStatisticsWidget::onSimulationFinished()
 {
-    ui->lineEdit_Cycles->setText(QString("%1").arg(cpu->getCycleCount()));
-    ui->lineEdit_Instructions->setText(QString("%1").arg(cpu->getInstructionCount()));
+    // Use locale so that strings have commas in them.
+    ui->lineEdit_Cycles->setText(QLocale::system().toString(cpu->getCycleCount()));
+    ui->lineEdit_Instructions->setText(QLocale::system().toString(cpu->getInstructionCount()));
     fillModel(cpu->getInstructionHistogram());
-    //ui->textBrowser_Histogrram->setText(cpu->getInstructionHistogram());
 }
+
+// POD class to help aggregate statistics.
+struct lookup {
+    // How many times an instruction was referenced.
+    quint32 tally;
+    // At what operand specifier does the instruction start.
+    quint8 start;
+    // How many addressing modes does this instruction have?
+    // Options are 0 (unary), 2(non-unary a field), and 8 (non-unary aaa field).
+    quint8 addrModes;
+};
 
 void ExecutionStatisticsWidget::fillModel(const QVector<quint32> histogram)
 {
+    // Make sure the model has no existing items.
+    // Model will make sure to delete any extra items
     model->removeRows(0, model->rowCount());
 
-    int tallyIt = 0;
-    Enu::EMnemonic mnemon = Enu::EMnemonic::STOP;
-    QList<quint32> tally = QList<quint32>();
-    QList<Enu::EMnemonic> mnemonicList;
+    Enu::EMnemonic mnemon;
+    QMap<Enu::EMnemonic, lookup> mnemonicMap;
 
-    mnemonicList << Enu::EMnemonic::STOP;
-    tally.append(0);
-
+    // Verify that the histogram contains enough entries to be read.
+    if(histogram.length() < 256) {
+        qWarning() << "Histogram is not long enough";
+        return;
+    }
+    // For every opcode from 00..FF
     for(int it = 0; it < 256; it++) {
-        if(mnemon == Pep::decodeMnemonic[it]) {
-            tally[tallyIt]+= histogram[it];
+        // Convert the int to an actual instruction
+        mnemon = Pep::decodeMnemonic[it];
+        // If the instruction exists, update the data in place
+        if(mnemonicMap.contains(mnemon)) {
+            mnemonicMap[mnemon].tally += histogram[it];
+            // If the item already exists, then it must be a non-unary instruction.
+            // Therefore, the first loop iteration was an addressing mode,
+            // and we must adjust the address mode counter to compensate.
+            if(mnemonicMap[mnemon].addrModes == 0) mnemonicMap[mnemon].addrModes += 1;
+            // Each time we match a mnemonic, we have another addressing mode for the same instruction.
+            mnemonicMap[mnemon].addrModes += 1;
         }
+        // Otherwsie create a new data object.
         else {
-            tally.append(histogram[it]);
-            tallyIt++;
-            mnemon = Pep::decodeMnemonic[it];
-            mnemonicList.append(mnemon);
+            lookup entry;
+            entry.start = it;
+            entry.tally = histogram[it];
+            // Assume a mnemonic is unary until otherwise proven.
+            entry.addrModes = 0;
+            mnemonicMap[mnemon] = entry;
         }
     }
-    static QMetaEnum metaenum = Enu::staticMetaObject.enumerator(Enu::staticMetaObject.indexOfEnumerator("EMnemonic"));
-    for(int it = 0; it < mnemonicList.size(); it++) {
-        QStandardItem* instrName = new QStandardItem(QString(metaenum.valueToKey((int) mnemonicList[it])).toLower());
+    // Metaobjects to help convert enums to QStrings.
+    static QMetaEnum mnemonicMetaenum = Enu::staticMetaObject.enumerator(Enu::staticMetaObject.indexOfEnumerator("EMnemonic"));
+    static QMetaEnum addrMetaenum = Enu::staticMetaObject.enumerator(Enu::staticMetaObject.indexOfEnumerator("EAddrMode"));
+
+    // Iteratre through the key, value pairs in the map.
+    for(auto kvPair = mnemonicMap.keyValueBegin(); kvPair != mnemonicMap.keyValueEnd(); ++kvPair) {
+        Enu::EMnemonic mnemon = (*kvPair).first;
+        auto tuple = (*kvPair).second;
+
+        // If the instruction was not used, do not insert its entry.
+        if(tuple.tally == 0 ) continue;
+
+        // Create entries for the mnemonic name
+        QStandardItem* instrName = new QStandardItem(QString(mnemonicMetaenum.valueToKey((int)mnemon)).toLower());
         QStandardItem* instrCount = new QStandardItem();
-        instrCount->setData(QVariant(tally[it]), Qt::DisplayRole);
+        // Make a variant from an int type to ensure that sorting works correctly.
+        instrCount->setData(QVariant(tuple.tally), Qt::DisplayRole);
         model->insertRow(model->rowCount(), {instrName, instrCount});
-        //if(Pep::addrModesMap[])
+
+        for(int offset = 0;  offset < tuple.addrModes; offset++) {
+            // If the addressing mode was not used, do not insert the entry.
+            if(histogram[tuple.start + offset] == 0) continue;
+            // Otherwise, for every opcode between the start and number of addressing modes,
+            // figure out the addressing mode associated with the instruction.
+            Enu::EAddrMode addr = Pep::decodeAddrMode[tuple.start + offset];
+            QStandardItem* addrName = new QStandardItem(QString(addrMetaenum.valueToKey((int) addr)).toLower());
+            QStandardItem* addrCount = new QStandardItem();
+            addrCount->setData(QVariant(histogram[tuple.start + offset]), Qt::DisplayRole);
+
+            instrName->appendRow({addrName, addrCount});
+        }
     }
 
 }
