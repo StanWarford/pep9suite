8e90d5ffcdfcc2d161321e196c1eb513d3a06b83
Matthew McRaven <mkm302@georgetown.edu>
2019-12-31 17:02:36 -0800

    Include a revision number in version (9.3 => 9.3.0).
    Multiple versions of Pep9Term will most likely be released, so a revision number will make it more clear to users which version of Pep9 they are on.

--- pep9micro/micromain.cpp
+++ pep9micro/micromain.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Pepperdine Computer Science Lab");
     QCoreApplication::setOrganizationDomain("cslab.pepperdine.edu");
     QCoreApplication::setApplicationName("Pep9Micro");
-    QCoreApplication::setApplicationVersion("9.3");
+    QCoreApplication::setApplicationVersion("9.3.0");
 
     MicroMainWindow w;
     w.show();
